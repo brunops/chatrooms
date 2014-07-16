7950e2ff27d87f2fcbf65aaa430628651ef4aa4a
Bruno Sanches <brunopsanches@gmail.com>
2014-07-16 12:50:22 -0700

    Serve static files using sendFile and serveStatic functions

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+var http = require('http'),
+    mime = require('mime'),
+    path = require('path'),
+    fs = require('fs');
+
+// cache file contents
+var cache = {};
 
 // Helper functions
 function send404(res) {
@@ -5,4 +12,36 @@ function send404(res) {
   res.write('Error 404: Resource not found.');
   res.end();
 }
+
+function sendFile(res, filePath, fileContents) {
+  res.writeHead(200, {
+    'Content-Type': mime.lookup(path.basename(filePath)),
+  });
+  res.end(fileContents);
+}
+
+function serveStatic(res, cache, absPath) {
+  if (cache[absPath]) {
+    sendFile(res, absPath, cache[absPath]);
+  }
+  else {
+    fs.exists(absPath, function (exists) {
+      if (exists) {
+        fs.readFile(absPath, function (err, data) {
+          if (err) {
+            send404(res);
+          }
+          else {
+            cache[absPath] = data;
+            sendFile(res, absPath, data);
+          }
+        });
+      }
+      else {
+        send404(res);
+      }
+    });
+  }
+}
+
 
